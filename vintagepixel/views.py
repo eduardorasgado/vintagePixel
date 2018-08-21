7bf904f8c6bdc4a88151fd108ea51d2537a26423
lal <eduardo.rasgado@hotmail.com>
2018-08-21 08:32:28 -0500

    json response to Hi in views

--- vintagepixel/views.py
+++ vintagepixel/views.py
@@ -3,7 +3,7 @@ Vistas
 """
 #Django
 # libreria importada para hello_world
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 #utilities
 from datetime import datetime
@@ -20,5 +20,22 @@ def hello_world(request):
 def hi(request):
     """Hi function"""
     # print("Hi request object:{0}".format(request))
-    import pdb; pdb.set_trace()
-    return HttpResponse("Hi")
+    # import pdb; pdb.set_trace()
+
+    # tomando los valores de los numbers enviados
+    # y obtenidos por get
+    if not 'numbers' in request.GET:
+        return HttpResponse("NO numbers")
+
+    numbers = request.GET['numbers']
+    # transformando string a lista
+    numbersList = numbers.split(",")
+    numbersInt = []
+    # convirtiendo cada valor string de la lista a int
+    for i in range(len(numbersList)):
+        numbersInt.append(int(numbersList[i]))
+    # ordenando esos numeros
+    newNumbersInt = sorted(numbersInt)
+
+    # retornando lista ordenada en formato Json
+    return JsonResponse({"numbers" : newNumbersInt}, safe=False)
