f53a92669eb855293c5f8d573d060107570c99d8
lal <eduardo.rasgado@hotmail.com>
2018-08-21 07:52:58 -0500

    showing time in hello_world view

--- vintagepixel/views.py
+++ vintagepixel/views.py
@@ -1,11 +1,18 @@
 """
 Vistas
 """
+#Django
 # libreria importada para hello_world
 from django.http import HttpResponse
+
+#utilities
+from datetime import datetime
 
 def hello_world(request):
     """
     El requet es obligatorio
     """
-    return HttpResponse('Hello Django :)')
+    now = datetime.now().strftime('%b %d %Y - %H:%M hrs')
+    now = str(now)
+    data = 'Hello Django :) Its {0}'.format(now)
+    return HttpResponse(data)
